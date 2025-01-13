e33296ffbba3565e9a9d053bf801e727f9de0a6f
celesca <folk.sawit@gmail.com>
2025-01-13 14:14:24 +0700

    add: models for comment and stock schema

--- Models/Stock.cs
+++ Models/Stock.cs
@@ -1,7 +1,16 @@
+using System.ComponentModel.DataAnnotations.Schema;
+
 namespace finshark.Models;
 
 public class Stock
 {
     public int Id { get; set; }
-    
+    public string Symbol { get; set; } = string.Empty;
+    public string CompanyName { get; set; } = string.Empty;
+    [Column(TypeName = "dicemail(18,2)")]
+    public decimal Purchase { get; set; }
+    public decimal LastDiv { get; set; }
+    public string Industry { get; set; } = string.Empty;
+    public long MarketCap { get; set; }
+    public List<Comment> Comments { get; set; } = new List<Comment>();
 }
